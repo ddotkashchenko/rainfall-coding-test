ec02246fbb931ff804d818872d3782acce795967
Your Name <dmytro.kashchenko@ihsmarkit.com>
2023-12-05 08:39:42 +0200

    init

---
+++ Rainfall.Api.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Rainfall.Api.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
